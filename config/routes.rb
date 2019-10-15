1f4fb8a1e48a99587f83a05e792873906b4539a9
Tanya <tatianadavydiuk@gmail.com>
2019-10-15 12:02:50 +0300

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+	root 'welcome#index'
+	get '/:locale', to: 'welcome#index'
+	# http://localhost:3000/?locale=pirate to switch languages
+  scope "/:locale" do
+  	get 'welcome/index'
+	end
+ end
