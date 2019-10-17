228caabda2045fe44ad2b11d902ae17343434a4e
Tanya <tatianadavydiuk@gmail.com>
2019-10-17 09:27:58 +0300

    check log info & web-console & debugger 'byebug'

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 	root 'welcome#index'
+	get 'welcome/index'
 	get '/:locale', to: 'welcome#index'
 	# http://localhost:3000/?locale=pirate to switch languages
-  scope "/:locale" do
-  	get 'welcome/index'
-	end
+ #  scope "/:locale" do
+ #  	get 'welcome/index'
+	# end
  end
