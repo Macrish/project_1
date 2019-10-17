3348aaaf12e3b5bfba4c3ebcfe4c72024038dcef
Tanya <tatianadavydiuk@gmail.com>
2019-10-17 09:27:58 +0300

    check log info & web-console & debugger 'byebug'

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,26 @@
 class WelcomeController < ApplicationController
   def index
+  	# check debbuger 'byebug'
+  	# byebug
+
+  	#web-consele exsecute Ruby code
+  	# console
+
   	flash[:notice] = t(:hello_flash)
+  	#logger info
+  	# logger.info "Processing the request..."
+  	# logger.fatal "Terminating application, raised unrecoverable error!!!"
+  	
+  	# tagged logging
+    # logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+		# logger.tagged("BCX") { logger.info "Stuff" }
+		# logger.tagged("BCX", "Jason") { logger.info "Stuff" }         
+		# logger.tagged("BCX") { logger.tagged("Jason") { logger.info "Stuff" } }
+		
+		# aaa = "Tanyaaaaaaaaaa"
+		#0.1s
+		# logger.debug "Person attributes hash: #{aaa}"
+		#0.0s more productive
+		# logger.debug { "Person attributes hash: #{aaa}" }
   end
 end
