5fce8739f81e10b89451b4792800fb13f8be4cae
Tatiana Leshchenko <t.m.leshchenko@gmail.com>
2019-10-24 11:42:08 +0300

    routes: match instead get & require match without methods(ArgumentError)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 	root 'welcome#index'
-	get 'welcome/index'
+
+	# get 'welcome/show'
+	#match without methods -> ArgumentError
+	# match 'welcome/show'
+	match 'welcome/show', to: 'welcome#show', via: [:get]
+
 	get '/:locale', to: 'welcome#index'
+
+	resources :posts
 	# http://localhost:3000/?locale=pirate to switch languages
  #  scope "/:locale" do
  #  	get 'welcome/index'
