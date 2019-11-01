43ee1be35677fed8c4235b7883c2e2aa24c155d3
Tanya Leshchenko <t.m.leshchenko@gmail.com>
2019-11-01 16:11:32 +0200

    add token to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ApplicationRecord
 	attr_readonly :first_name
+	#console
+	#user = User.create
+	#user.token
+
+	before_create :generate_token
+
+	private
+
+		def generate_token
+			self.token = loop do
+				t = SecureRandom.hex(16)
+				break t unless User.where(token: t).exists?
+			end
+		end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_01_075824) do
+ActiveRecord::Schema.define(version: 2019_11_01_140831) do
 
   create_table "events", force: :cascade do |t|
     t.date "starts_at"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_11_01_075824) do
     t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "token"
   end
 
 end
