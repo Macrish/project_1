27d8529fb5723f5638daf12838f46089027beffa
Tanya Leshchenko <t.m.leshchenko@gmail.com>
2019-11-07 09:16:40 +0200

    using Accept-Language (* Locale set to 'en'  (in log))

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
 	around_action :switch_locale
 
 	def default_url_options
@@ -9,4 +10,16 @@ class ApplicationController < ActionController::Base
     locale = params[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
   end
+
+	#* Locale set to 'en' in log	
+	def set_locale
+		I18n.locale = extract_locale_from_accept_language_header
+		logger.debug "* Locale set to '#{I18n.locale}'"
+	end
+	
+	private
+
+		def extract_locale_from_accept_language_header
+			request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+		end
 end
